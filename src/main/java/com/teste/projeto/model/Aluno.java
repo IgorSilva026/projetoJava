1c0aada925b2da906d5e7048d4b672c950940922
IgorSilva026 <igorssilva026@gmail.com>
2025-04-12 11:17:41 -0300

    feat: "Professores"

--- src/main/java/com/teste/projeto/model/Aluno.java
+++ src/main/java/com/teste/projeto/model/Aluno.java
@@ -1,9 +1,49 @@
 package com.teste.projeto.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 public class Aluno {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String nomeAluno;
-    private long idAluno;
+    private long Id;
     private String matriculaAluno;
     private int idadeAluno;
+
+
+    //getter e setter alunos
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        this.Id = id;
+    }
+
+    public String getMatriculaAluno() {
+        return matriculaAluno;
+    }
+
+    public void setMatriculaAluno(String matriculaAluno) {
+        this.matriculaAluno = matriculaAluno;
+    }
+
+    public int getIdadeAluno() {
+        return idadeAluno;
+    }
+
+    public void setIdadeAluno(int idadeAluno) {
+        this.idadeAluno = idadeAluno;
+    }
 }
--- src/main/java/com/teste/projeto/model/Professor.java
+++ src/main/java/com/teste/projeto/model/Professor.java
@@ -1,4 +1,38 @@
 package com.teste.projeto.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        this.Id = id;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    }
+
+    public void setDisciplinaProfessor(String disciplinaProfessor) {
+        this.disciplinaProfessor = disciplinaProfessor;
+    }
 }
