a481d3a265e82cd2ccbdbc6f394992ba740d1c3c
IgorSilva026 <igorssilva026@gmail.com>
2025-04-12 10:35:31 -0300

    feat: "Alunos"

---
+++ src/main/java/com/teste/projeto/model/Aluno.java
@@ -0,0 +1,9 @@
+package com.teste.projeto.model;
+
+public class Aluno {
+
+    private String nomeAluno;
+    private long idAluno;
+    private String matriculaAluno;
+    private int idadeAluno;
+}
